c878464bc0a5d5f665429ab5ac965026610d5442
Brunnosilva <brunowolks26@gmail.com>
2020-06-27 18:44:20 -0300

    colocando persogem principal

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 let imagemCenario; 
+let imagemPersonagem;
 
 function preload(){ 
 imagemCenario = loadImage('imagens/cenario/floresta.png');
+imagemPersonagem = loadImage('imagens/personagem/correndo.png');
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -10,4 +12,5 @@ function setup() {
 
 function draw() {
   background(imagemCenario);
+  image(imagemPersonagem,0 ,height -135 ,110 ,135 , 0 ,0 ,220, 270 );
 }
